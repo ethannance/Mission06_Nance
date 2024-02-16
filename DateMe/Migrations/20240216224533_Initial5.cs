53956bf2425a608366d722184f08da103c4a5369
ethannance <ethannance523@gmail.com>
2024-02-16 15:51:53 -0700

    Normalized some more

---
+++ DateMe/Migrations/20240216224533_Initial5.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mission06_Nance.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Category",
+                table: "Applications",
+                newName: "Main_Category");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Main_Category",
+                table: "Applications",
+                newName: "Category");
+        }
+    }
+}
