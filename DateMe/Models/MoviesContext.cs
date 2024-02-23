413f758bbd2eaf49f14ac2142091bf92ec445c83
ethannance <ethannance523@gmail.com>
2024-02-23 11:45:47 -0700

    Removed unnecessary stuff

--- DateMe/Models/MoviesContext.cs
+++ DateMe/Models/MoviesContext.cs
@@ -11,19 +11,6 @@ namespace Mission06_Nance.Models
         public DbSet<Application> Movies { get; set; }
         public DbSet<CategoryCategory> Categories { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder) //Seed data
-        {
-            modelBuilder.Entity<CategoryCategory>().HasData(
 
-                new CategoryCategory { CategoryId = 1, Category= "Miscellaneous"},
-                new CategoryCategory { CategoryId = 2, Category = "Drama" },
-                new CategoryCategory { CategoryId = 3, Category = "Television" },
-                new CategoryCategory { CategoryId = 4, Category = "Horror/Suspense" },
-                new CategoryCategory { CategoryId = 5, Category = "Comedy" },
-                new CategoryCategory { CategoryId = 6, Category = "Family" },
-                new CategoryCategory { CategoryId = 7, Category = "Action/Adventure" },
-                new CategoryCategory { CategoryId = 8, Category = "VHS" }
-                );
-        }
     }
 }
